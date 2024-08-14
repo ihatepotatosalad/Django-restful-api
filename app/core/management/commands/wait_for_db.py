42fe7e73301e7e294783c1f565ceda79463f5640
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2024-08-13 23:41:31 -0400

    added pre tests

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""DJANGO COMMAND TO WAIT FOR DB TO BE AVALIBLE"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """COMMAND TO WAIT FOR DJANGOP TO WAIT FOR DATABASE"""
+
+    def handle(self, *args, **options):
+        pass
