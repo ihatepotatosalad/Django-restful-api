f5873c71da039c53efcbc4a1d9f0bf44d35eab16
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2024-08-20 20:30:42 -0400

    linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -8,4 +8,4 @@ def add(a, b):
 
 
 def sub(a, b):
-    return a-b
+    return a - b
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -89,7 +89,7 @@ class PublicUserAPITest(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_create_token_for_user(self):
+    def test_create_token_for_user_no_password(self):
         test_user = {
             'email': 'test@example.com',
             'name': 'test',
